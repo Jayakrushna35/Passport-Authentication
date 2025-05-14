8aae10838b65f3a0bfab1e7c481253fc5f66ff50
Jayakrushna35 <jayakrushnapattnaik6@gmail.com>
2025-05-14 11:58:21 +0530

    Server is running

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT ,() => {
     console.log(`Server is running on port ${PORT}`)
